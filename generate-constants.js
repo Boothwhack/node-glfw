2ffda9b14671a0e3aa03f28f91f99c186cfeb8bc
Karoline <boothwhack@users.noreply.github.com>
2021-09-08 00:42:18 +0200

    Generate TypeScript GLFW constants definitions

--- generate-constants.js
+++ generate-constants.js
@@ -1,22 +1,35 @@
 const FS = require("fs/promises");
 
-if (process.argv.length < 5) throw new Error("Expected 3 arguments.");
+if (process.argv.length < 5) throw new Error("Expected 5 arguments.");
 
-const [glfwHeader, srcFile, generatedFile] = process.argv.slice(2);
+const [glfwHeader, cppSrcFile, generatedCppFile, tsSrcFile, generatedTsFile] = process.argv.slice(2);
 
-if (!srcFile.endsWith(".in")) throw new Error("Source file must end with '.in'.");
+if (!cppSrcFile.endsWith(".in")) throw new Error("Source file must end with '.in'.");
 
 const DefineConstant = /^#define\s+GLFW_(\w+)/gm;
 
 (async () => {
     const header = (await FS.readFile(glfwHeader)).toString();
 
-    const constantDescriptors = Array.from(header.matchAll(DefineConstant))
+    const constants = Array.from(header.matchAll(DefineConstant));
+
+    // Generate C++ constants
+    const constantDescriptors = constants
         .map(([_, name]) => `ConstantDescriptor(${name})`)
-        .join(',\n');
+        .join(",\n");
 
-    const src = (await FS.readFile(srcFile)).toString();
-    const generatedSrc = src.replace("{{CONSTANTS}}", constantDescriptors);
+    const cppSrc = (await FS.readFile(cppSrcFile)).toString();
+    const generatedCppSrc = cppSrc.replace("{{CONSTANTS}}", constantDescriptors);
 
-    await FS.writeFile(generatedFile, generatedSrc);
+    await FS.writeFile(generatedCppFile, generatedCppSrc);
+
+    // Generate TypeScript constant definitions
+    const constantDefinitions = constants
+        .map(([_,name]) => `export const ${name}: number;`)
+        .join('\n');
+
+    const tsSrc = (await FS.readFile(tsSrcFile)).toString();
+    const generatedTsSrc = tsSrc.replace("{{CONSTANTS}}", constantDefinitions);
+
+    await FS.writeFile(generatedTsFile, generatedTsSrc);
 })().catch(console.error);
