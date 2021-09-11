bd82105111c51db823de60112698625ad1858ae2
Karoline <boothwhack@users.noreply.github.com>
2021-09-11 22:35:49 +0200

    Fix module resolution

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {resolve} = require("path");
 const {arch, platform} = require("os");
 const GLFW = require(
     resolve(
-        ".", 
+        __dirname,
         "dist",
         process.env.NODE_ENV === "development" ? "Debug" : "Release",
         platform(),
