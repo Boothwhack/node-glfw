e4b6b0c7b13677b32d5194ed1c37cf7c597c2d17
Karoline <boothwhack@users.noreply.github.com>
2021-12-21 14:22:21 +0100

    Support GLFW being used from multiple threads

--- src/addon.cpp
+++ src/addon.cpp
@@ -1,9 +1,45 @@
 #include <napi.h>
 #include <GLFW/glfw3.h>
+#include <memory>
 
 #include "utils.hpp"
 #include "constants.hpp"
 #include "window.hpp"
+
+// GLFWAddon may be instantiated in multiple threads, but glfwInit should only be called once and glfwTerminate should
+// be called when all instances of GLFWAddon are destroyed.
+struct GLFWGlobalState {
+    GLFWGlobalState() {
+        glfwInit();
+    }
+
+    ~GLFWGlobalState() {
+        glfwTerminate();
+    }
+
+    static std::weak_ptr<GLFWGlobalState>& weakRefGlobalInstance() {
+        static std::weak_ptr<GLFWGlobalState> globalInstanceRef{};
+        return globalInstanceRef;
+    }
+
+    static std::shared_ptr<GLFWGlobalState> refGlobalInstance() {
+        static std::mutex mtx{};
+
+        // ensure multiple threads are not trying to create a new instance at the same time.
+        std::lock_guard guard{mtx};
+
+        auto& weakRef{weakRefGlobalInstance()};
+        if (weakRef.expired()) {
+            // create new instance and keep track of it, return it as the first reference.
+            auto newInstance{std::make_shared<GLFWGlobalState>()};
+            weakRef = newInstance;
+            return newInstance;
+        } else {
+            // at least one thread already has a reference to the instance, return another reference.
+            return weakRef.lock();
+        }
+    }
+};
 
 class GLFWAddon : public Napi::Addon<GLFWAddon> {
 public:
@@ -11,12 +47,10 @@ public:
     typedef glFunc (glLoadFunc)(const char*);
 
     glLoadFunc* loadPtr_;
+    std::shared_ptr<GLFWGlobalState> glfw_;
 
     GLFWAddon(Napi::Env env, Napi::Object exports)
-        : loadPtr_{&glfwGetProcAddress}
-    {
-        glfwInit();
-
+            : loadPtr_{&glfwGetProcAddress}, glfw_{GLFWGlobalState::refGlobalInstance()} {
         InitConstants(env, exports);
         InitWindow(env, exports);
 
@@ -27,13 +61,7 @@ public:
         });
     }
 
-    ~GLFWAddon()
-    {
-        glfwTerminate();
-    }
-
-    void pollEvents(const Napi::CallbackInfo& info)
-    {
+    void pollEvents(const Napi::CallbackInfo& info) {
         glfwPollEvents();
     }
 
